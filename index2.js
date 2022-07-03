a33857b0b229c6e675426d5d0a96ef64d9b72164
Kateryna Tsygankova <108408110+tsyganek@users.noreply.github.com>
2022-07-03 23:49:54 +0200

    Update index2.js
    add current city and weather, add current weather in user city

--- index2.js
+++ index2.js
@@ -61,102 +61,85 @@ function getCurrentDate() {
 getCurrentDate();
 
 //CITY
+
 function nameCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  if (cityInput.value.length > 1) {
-    document.querySelector("#city-name").innerHTML = cityInput.value;
-  } else {
-    alert(`it's not even a city! try again`);
+  document.querySelector("#city-name").innerHTML = cityInput.value;
+
+  let apiKey = "294a89684f421e2c70cadf6f7a72b25c";
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=${units}&appid=${apiKey}`;
+
+  function showCityWeather(response) {
+    let currentTempElement = document.getElementById("temp-now");
+    let temperature = Math.round(response.data.main.temp);
+    currentTempElement.innerHTML = `${temperature}°C`;
   }
+  axios.get(url).then(showCityWeather);
 }
 
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", nameCity);
 
-//CURRENT TEMPERATURE
+//SWITCH DEGREES
 
-// function showCityWeather(response) {
-//   let currentTempElement = document.getElementById("temp-now");
-//   let temperature = Math.round(response.data.main.temp);
-//   currentTempElement.innerHTML = `${temperature}°C`;
-// }
+let temps = document.getElementsByClassName("temp");
 
-// function showPosition(position) {
-//   let apiKey = "294a89684f421e2c70cadf6f7a72b25c";
-//   let cityElement = document.querySelector("h1");
-//   let city = h1.value;
-//   console.log(city);
-//   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function handleChange(target) {
+  for (let i = 0; i < temps.length; i++) {
+    let tempNum = Number(temps[i].innerHTML.slice(0, 2));
+    let tempNumC = Math.round((tempNum - 32) / 1.8);
+    let tempNumF = Math.round(tempNum * 1.8 + 32);
+    switch (target.id) {
+      case "flexRadioDefault2":
+        temps[i].innerHTML = `${tempNumF}°F`;
+        break;
+      case "flexRadioDefault1":
+        temps[i].innerHTML = `${tempNumC}°C`;
+        break;
+      default:
+        temps[i].innerHTML = `${tempNum}°C`;
+        break;
+    }
+  }
+}
+let radios = document.getElementsByName("flexRadioDefault");
+handleChange(radios);
 
-//   axios.get(url).then(showWeather);
-// }
+// CURRENT BUTTON
 
-// let temps = document.getElementsByClassName("temp");
+function showWeather(response) {
+  let currentTempElement = document.getElementById("temp-now");
+  let temperature = Math.round(response.data.main.temp);
+  currentTempElement.innerHTML = `${temperature}°C`;
+}
 
-// function handleChange(target) {
-//   for (let i = 0; i < temps.length; i++) {
-//     let tempNum = Number(temps[i].innerHTML.slice(0, 2));
-//     let tempNumC = Math.round((tempNum - 32) / 1.8);
-//     let tempNumF = Math.round(tempNum * 1.8 + 32);
-//     switch (target.id) {
-//       case "flexRadioDefault2":
-//         temps[i].innerHTML = `${tempNumF}°F`;
-//         break;
-//       case "flexRadioDefault1":
-//         temps[i].innerHTML = `${tempNumC}°C`;
-//         break;
-//       default:
-//         temps[i].innerHTML = `${tempNum}°C`;
-//         break;
-//     }
-//   }
-// }
-// let radios = document.getElementsByName("flexRadioDefault");
-// handleChange(radios);
+function retrievePosition(position) {
+  let apiKey = "294a89684f421e2c70cadf6f7a72b25c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url1 = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`;
 
-//city name
+  function showCityData(response) {
+    let currentCity = response.data[0].name;
+    document.querySelector("#city-name").innerHTML = currentCity;
 
-// function showCityWeather(response) {
-//   let currentTempElement = document.getElementById("temp-now");
-//   let temperature = Math.round(response.data);
-//   console.log(temperature);
+    let url2 = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${apiKey}`;
 
-//   currentTempElement.innerHTML = `${temperature}°C`;
-//   let title = document.querySelector("h1");
-//   let cityname = title.innerHTML;
-//   // console.log(`in ${cityname} is ${temperature}`);
-//   let apiKey = "294a89684f421e2c70cadf6f7a72b25c";
-//   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metrics&appid=${apiKey}`;
+    function showCityWeather(response) {
+      let currentTempElement = document.getElementById("temp-now");
+      let temperature = Math.round(response.data.main.temp);
+      currentTempElement.innerHTML = `${temperature}°C`;
+    }
 
-//   axios.get(url).then(showCityWeather);
-// }
+    axios.get(url2).then(showCityWeather);
+  }
+  axios.get(url1).then(showCityData);
+}
 
-// let btn = document.querySelector("button");
-// btn.addEventListener("click", showCityWeather);
-
-//current
-
-// function showWeather(response) {
-//   let currentTempElement = document.getElementById("temp-now");
-//   let temperature = Math.round(response.data.main.temp);
-//   currentTempElement.innerHTML = `${temperature}°C`;
-// }
-
-// function retrievePosition(position) {
-//   let apiKey = "294a89684f421e2c70cadf6f7a72b25c";
-//   let lat = position.coords.latitude;
-//   let lon = position.coords.longitude;
-//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-
-//   axios.get(url).then(showWeather);
-// }
-
-// navigator.geolocation.getCurrentPosition(retrievePosition);
-
-// currentBtn = document.querySelector("#currentbtn");
-
-// currentBtn.addEventListener(
-//   "click",
-//   navigator.geolocation.getCurrentPosition(retrievePosition)
-// );
+currentBtn = document.querySelector("#currentbtn");
+currentBtn.addEventListener(
+  "click",
+  navigator.geolocation.getCurrentPosition(retrievePosition)
+);
